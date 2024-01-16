b6a7d7c66be3fb73478ebea938fcb906f6740d70
Rafael Barbosa <64490454+RafaelBarbosa0@users.noreply.github.com>
2024-01-16 03:23:11 +0000

    Stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.XR.CoreUtils;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
